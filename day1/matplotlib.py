136efb322b9b2856e9f7744984f9b9fca3eb67c4
eswaribala <parameswaribala@gmail.com>
2022-06-01 13:14:34 +0530

    updated day1

---
+++ day1/matplotlib.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 13:11:06 2022
+
+@author: Balasubramaniam
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv("diabetes.csv")
+x=df["BMI"]
+y=df["Glucose"]
+y1=df["BloodPressure"]
+plt.scatter(x, y, color='g')
+plt.scatter(x, y1, color='m')
+plt.show()
