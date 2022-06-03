609016fa68c629e95aa19066b964dad7d51320f1
eswaribala <parameswaribala@gmail.com>
2022-06-03 11:42:44 +0530

    updated

--- day3/polynomialregression/polynomial_regression.py
+++ day3/polynomialregression/polynomial_regression.py
@@ -8,31 +8,56 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+y = dataset.iloc[:, 2:3].values
+
 
 # Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#y_train=np.array(y_train).reshape(-1,1)
+#y_test=np.array(y_test).reshape(-1,1)
+
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+'''
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
+X_test = sc_X.transform(X_test)
+y_train = sc_X.fit_transform(y_train)
+y_test = sc_X.transform(y_test)
+
+print(X_train,y_train)
+print(X_test,y_test)
+'''
 
 # Fitting Linear Regression to the dataset
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+lin_reg.fit(X_train, y_train)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
-print(X_poly)
+X_poly = poly_reg.fit_transform(X_train)
+
 #poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
+lin_reg_2.fit(X_poly, y_train)
+
+
+
+# Predicting a new result with Linear Regression
+y_pred=lin_reg.predict(X_test)
+print("Linear yactual,ypredict")
+print(y_test)
+print(y_pred)
+# Predicting a new result with Polynomial Regression
+poly_pred=lin_reg_2.predict(poly_reg.fit_transform(X_test))
+print("Polynomial yactual,ypredict")
+print(y_test)
+print(poly_pred)
 
 # Visualising the Linear Regression results
 plt.scatter(X, y, color = 'red')
@@ -44,7 +69,7 @@ plt.show()
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'green')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
@@ -60,7 +85,4 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Predicting a new result with Linear Regression
-print(lin_reg.predict(6.5))
-# Predicting a new result with Polynomial Regression
-print(lin_reg_2.predict(poly_reg.fit_transform(6.5)))
+
