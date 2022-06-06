5519cd10deb320a47cbb9c61686c112e160148cf
eswaribala <parameswaribala@gmail.com>
2022-06-06 09:39:50 +0530

    updated

--- day3/svr/svr.py
+++ day3/svr/svr.py
@@ -10,10 +10,14 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2:3].values
 
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+print(X)
+print(y)
 
+# Splitting the dataset into the Training set and Test set
+'''
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+'''
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -21,21 +25,27 @@ sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
+print(X)
+print(y)
+
 # Fitting SVR to the dataset
 from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
+
+'''
 import statsmodels.api as sm
 
 est = sm.OLS(y, X) #ordinary least square method 
 est2 = est.fit()
 print("Summary.....")
 print(est2.summary())
-
+'''
 # Predicting a new result
-y_pred = regressor.predict(6.5)
-y_pred = sc_y.inverse_transform(y_pred)
+y_pred = regressor.predict([[6.5]])
+print(y_pred)
+#y_pred = y.inverse_transform(y_pred)
 
 # Visualising the SVR results
 plt.scatter(X, y, color = 'red')
